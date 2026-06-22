17802e7b142877f3ad08250fa34f0d4ecd41a401
Guilherme Lopes <guilherme.lopes3@academico.ufpb.br>
2026-06-22 18:29:46 -0300

    feat(ddl): adiciona check constraints de dominio para cpf e grupo sanguineo

--- sql/ddl/02_pessoa.sql
+++ sql/ddl/02_pessoa.sql
@@ -5,5 +5,8 @@ CREATE TABLE PESSOA (
     data_nascimento DATE NOT NULL,
     is_flamengo BOOLEAN NOT NULL DEFAULT TRUE, -- importantíssimo!!!! :)
     telefone VARCHAR(20),
-    CONSTRAINT chk_cpf_length CHECK (length(cpf) = 11)
+    CONSTRAINT chk_cpf_length CHECK (length(cpf) = 11),
+    -- constraint de validação do tamanho exato e apenas caracteres numéricos
+    CONSTRAINT chk_cpf_formato CHECK (cpf ~ '^[0-9]{11}$'),
+    CONSTRAINT chk_data_nascimento_passada CHECK (data_nascimento <= CURRENT_DATE)
 );
--- sql/ddl/03_paciente.sql
+++ sql/ddl/03_paciente.sql
@@ -2,5 +2,6 @@ CREATE TABLE PACIENTE (
     id_pessoa UUID PRIMARY KEY REFERENCES PESSOA(id_pessoa) ON DELETE CASCADE,
     num_convenio VARCHAR(50),
     alergias TEXT,
-    grupo_sanguineo VARCHAR(3)
+    grupo_sanguineo VARCHAR(3),
+    CONSTRAINT chk_grupo_sanguineo_valido CHECK (grupo_sanguineo IN ('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'))
 );
